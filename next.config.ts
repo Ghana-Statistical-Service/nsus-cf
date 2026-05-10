868c7f03e826bc8e09c7e82f584b69ae16654912
Afisu Ganiyu <72135122+iamafisu@users.noreply.github.com>
2026-05-10 03:36:37 +0000

    Fix Tailwind v4 PostCSS config

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'store.statsghana.gov.gh',
-  //     },
-  //   ],
-  // },
-
 };
 
 export default nextConfig;
